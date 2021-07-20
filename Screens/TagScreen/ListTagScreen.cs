a5962f9922cebd38108b3842497edaf5003f2965
VictorSardellaro <victor.henryk@outlook.com>
2021-07-20 11:20:28 -0300

    SharingBDConnection

--- Screens/TagScreen/ListTagScreen.cs
+++ Screens/TagScreen/ListTagScreen.cs
@@ -1,4 +1,6 @@
 using System;
+using Blog.Models;
+using Blog.Repositories;
 
 namespace Blog.Screens.TagScreens
 {
@@ -6,6 +8,21 @@ namespace Blog.Screens.TagScreens
     {
         public static void Load()
         {
+            Console.Clear();
+            System.Console.WriteLine("Lista de tags");
+            System.Console.WriteLine("------------------");
+            List();
+            Console.ReadKey();
+        }
+
+        private static void List()
+        {
+            var respository = new Repository<Tag>(Database.Connection);
+            var tags = respository.Get();
+            foreach (var item in tags)
+            {
+                System.Console.WriteLine($"{item.Id} - {item.Name} ({item.Slug})");
+            }
 
         }
 
