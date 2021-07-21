9066a66fe811d270bd62f7aac5c10fb0c85d8683
VictorSardellaro <victor.henryk@outlook.com>
2021-07-21 16:52:27 -0300

    ExplicitKey_Documentation

--- Models/UserRole.cs
+++ Models/UserRole.cs
@@ -6,11 +6,11 @@ namespace Blog.Models
     [Table("[UserRole]")]
     public class UserRole
     {
+        //https://stackoverflow.com/questions/22462987/dapper-insert-into-table-that-has-a-composite-pk
         [ExplicitKey]
         public int UserId { get; set; }
         [ExplicitKey]
         public int RoleId { get; set; }
-
 
     }
 }
